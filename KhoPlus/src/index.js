340e88af21c812ed016926bc3c5b32e8a15621a5
Trong Nguyen <39550097+trongnguyen9966@users.noreply.github.com>
2024-04-15 18:03:17 +0700

    Update index.js

--- KhoPlus/src/index.js
+++ KhoPlus/src/index.js
@@ -7,27 +7,34 @@ import AppStack from "./router/appScreen";
 import store from "./state/store";
 
 import { RootSiblingParent } from "react-native-root-siblings";
+import { screenName } from "./router/screenName";
 export default function AppComponent(props) {
   const navigationContainer = useRef(null);
   useEffect(() => {
     const resp = navigationContainer?.current?.getCurrentRoute();
-    const backAction = () => {
-      Alert.alert("Thông báo", "Bạn có chắc chắn muốn thoát khỏi ứng dụng ?", [
-        {
-          text: "Hủy",
-          onPress: () => null,
-          style: "cancel",
-        },
-        { text: "Thoát", onPress: () => BackHandler.exitApp() },
-      ]);
-      return true;
-    };
+    if (resp == screenName.AUTH_APP) {
+      const backAction = () => {
+        Alert.alert(
+          "Thông báo",
+          "Bạn có chắc chắn muốn thoát khỏi ứng dụng ?",
+          [
+            {
+              text: "Hủy",
+              onPress: () => null,
+              style: "cancel",
+            },
+            { text: "Thoát", onPress: () => BackHandler.exitApp() },
+          ]
+        );
+        return true;
+      };
 
-    const backHandler = BackHandler.addEventListener(
-      "hardwareBackPress",
-      backAction
-    );
-    return () => backHandler.remove();
+      const backHandler = BackHandler.addEventListener(
+        "hardwareBackPress",
+        backAction
+      );
+      return () => backHandler.remove();
+    }
   }, []);
 
   return (
